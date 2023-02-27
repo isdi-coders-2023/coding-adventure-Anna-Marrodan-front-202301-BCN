8cf7e89d6384bd606b993811182e5ff07ee81605
Anna Marrodán <anna.marrodan@gmail.com>
2023-02-27 16:12:06 +0100

    Test Button component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "./components/Button/Button";
 
 const App = (): JSX.Element => {
-  return <Button text="Sign up" />;
+  return <Button text={"Sign up"}></Button>;
 };
 
 export default App;
---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Button from "./Button";
+
+describe("Given a Button component", () => {
+  describe("When it is rendered with the text 'Sign up'", () => {
+    test("Then it should show a button", () => {
+      const expectedText = "Sign up";
+
+      render(<Button text={expectedText}></Button>);
+
+      const button = screen.getByRole("button", { name: expectedText });
+      expect(button).toBeInTheDocument();
+    });
+  });
+});
