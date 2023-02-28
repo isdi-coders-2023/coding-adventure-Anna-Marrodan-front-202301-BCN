13d0db8dab17ee7bdaeda7573738ce54db7121ce
Anna Marrodán <anna.marrodan@gmail.com>
2023-02-28 16:08:14 +0100

    Modify BEM in required text and delete title in Form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,12 +3,13 @@ import Button from "../Button/Button";
 export const Form = (): JSX.Element => {
   return (
     <>
-      <h2>Register for the event</h2>
       <form className="register-form">
         <div>
           <label htmlFor="email">Email</label>
           <input type="email" placeholder="Introduce your email" id="email" />
-          <span className="form__required-text">Email is required</span>
+          <span className="register-form__required-text">
+            Email is required
+          </span>
         </div>
         <div>
           <label htmlFor="password">Password</label>
@@ -17,12 +18,16 @@ export const Form = (): JSX.Element => {
             placeholder="Introduce your password"
             id="password"
           />
-          <span className="form__required-text">Password is required</span>
+          <span className="register-form__required-text">
+            Password is required
+          </span>
         </div>
         <div>
           <label htmlFor="image">Image</label>
           <input type="file" placeholder="Introduce an image" id="image" />
-          <span className="form__required-text">Image is required</span>
+          <span className="register-form__required-text">
+            Image is required
+          </span>
         </div>
         <Button text="Sign up" />
       </form>
