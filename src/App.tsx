b31b85d7bcaa94e42e828b9f42be7d85e460debe
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-01 17:37:57 +0100

    Delete unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { Form } from "./components/Form/Form";
 
 const App = (): JSX.Element => {
-  return (
-    <>
-      <Form submitForm={() => {}} />
-    </>
-  );
+  return <></>;
 };
 
 export default App;
